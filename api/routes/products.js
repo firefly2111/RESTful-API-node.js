b54d0c158efc36036cb653d8b77e3de057c6496d
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-03-19 00:21:40 +0100

    Add files via upload

---
+++ api/routes/products.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const router = express.Router();
+const mongoose = require("mongoose");
+const multer = require("multer");
+const Product = require("../models/product");
+const checkAuth = require("../middleware/check-auth");
+const ProductsController = require("../controllers/products");
+ 
+const storage = multer.diskStorage({
+	destination: function(req,file,cb){
+		cb(null, "./uploads/");
+	},
+	filename: function(req,file,cb){
+		cb(null, new Date().toIOSString() + file.originalname);
+	}
+});
+const fileFilter = (req,file,cb) => {
+	if(file.mimetype === "image/jpeg" || file.mimetype === "image/png"){
+		cb(null, true);
+	}else{
+		cb(null, false);
+	}
+};
+const upload = multer({storage: storage,
+				limits: {fileSize: 1024 * 1024 * 5},
+				fileFilter: fileFilter});
+
+router.get("/", ProductsController.products_get_all);
+
+router.post("/", checkAuth, upload.single("productImage"), ProductsController.products_create_product);
+
+
+router.get("/:productId", ProductsController.products_get_product);
+
+router.patch("/:productId", checkAuth, ProductsController.products_update_product);
+
+router.delete("/:productId", checkAuth, ProductsController.product_delete);
+
+module.exports = router;
---
+++ api/routes/user.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserController = require("../controllers/user");
+const checkAuth = require("../middleware/check-auth");
+
+router.post("/signup", UserController.user_signup);
+
+router.post("/login", UserController.user_login);
+
+router.delete("/:userId", checkAuth, UserController.user_delete);
+
+
+
+module.exports = router;
